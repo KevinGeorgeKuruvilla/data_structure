215cebee56cb98662bfca70cd15e49ec7712a892
kevin george <kevingeorgekuruvila@gmail.com>
2025-08-30 19:07:43 +0530

    added-lr-srh

--- array.py
+++ array.py
@@ -8,3 +8,19 @@ for i in range(len(arr)):
 arr.insert(2,25)
 print("\nAfter insertion:",arr)
 print(arr)
+
+# deletion
+arr.remove(30)
+print("After deletion:",arr)
+
+# Linear search
+
+target=int(input("Enter the element to be searched:"))
+def linear_seasrch(arr,target):
+	for i in range(len(arr)):
+		if arr[i]==target:
+			return i
+	return -1
+
+# result=linear_seasrch(arr,target)
+print(linear_seasrch(arr,target));
